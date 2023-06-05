2666b5f660c64d699a0be440c3701c7be00ec309
Euler Taveira <euler.taveira@enterprisedb.com>
2023-06-05 14:39:40 -0400

    Creates a new logical replica from a standby server
    A new tool called pg_createsubscriber can convert a physical replica into a
    logical replica. It runs on the target server and should be able to
    connect to the source server (publisher) and the target server
    (subscriber).
    
    The conversion requires a few steps. Check if the target data directory
    has the same system identifier than the source data directory. Stop the
    target server if it is running as a standby server. Create one
    replication slot per specified database on the source server. One
    additional replication slot is created at the end to get the consistent
    LSN (This consistent LSN will be used as (a) a stopping point for the
    recovery process and (b) a starting point for the subscriptions). Write
    recovery parameters into the target data directory and start the target
    server (Wait until the target server is promoted). Create one
    publication (FOR ALL TABLES) per specified database on the source
    server. Create one subscription per specified database on the target
    server (Use replication slot and publication created in a previous step.
    Don't enable the subscriptions yet). Sets the replication progress to
    the consistent LSN that was got in a previous step. Enable the
    subscription for each specified database on the target server.
    Stop the target server. Change the system identifier from the target
    server.
    
    Depending on your workload and database size, creating a logical replica
    couldn't be an option due to resource constraints (WAL backlog should be
    available until all table data is synchronized). The initial data copy
    and the replication progress tends to be faster on a physical replica.
    The purpose of this tool is to speed up a logical replica setup.

---
+++ src/bin/pg_basebackup/t/040_pg_createsubscriber.pl
@@ -0,0 +1,44 @@
+# Copyright (c) 2024, PostgreSQL Global Development Group
+
+#
+# Test checking options of pg_createsubscriber.
+#
+
+use strict;
+use warnings;
+use PostgreSQL::Test::Utils;
+use Test::More;
+
+program_help_ok('pg_createsubscriber');
+program_version_ok('pg_createsubscriber');
+program_options_handling_ok('pg_createsubscriber');
+
+my $datadir = PostgreSQL::Test::Utils::tempdir;
+
+command_fails(['pg_createsubscriber'],
+	'no subscriber data directory specified');
+command_fails(
+	[
+		'pg_createsubscriber',
+		'--pgdata', $datadir
+	],
+	'no publisher connection string specified');
+command_fails(
+	[
+		'pg_createsubscriber',
+		'--dry-run',
+		'--pgdata', $datadir,
+		'--publisher-server', 'dbname=postgres'
+	],
+	'no subscriber connection string specified');
+command_fails(
+	[
+		'pg_createsubscriber',
+		'--verbose',
+		'--pgdata', $datadir,
+		'--publisher-server', 'dbname=postgres',
+		'--subscriber-server', 'dbname=postgres'
+	],
+	'no database name specified');
+
+done_testing();
