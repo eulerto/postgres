092ab0381ebd49188a90f9cd55216674c1a76933
Euler Taveira <euler.taveira@enterprisedb.com>
2023-10-10 21:14:22 -0300

    fix: improve basic test

--- src/bin/pg_subscriber/t/001_basic.pl
+++ src/bin/pg_subscriber/t/001_basic.pl
@@ -1,13 +1,11 @@
 # Copyright (c) 2023, PostgreSQL Global Development Group
 
+#
+# Test checking options of pg_subscriber.
+#
+
 use strict;
 use warnings;
-use Cwd;
-use Config;
-use File::Basename qw(basename dirname);
-use File::Path qw(rmtree);
-use Fcntl qw(:seek);
-use PostgreSQL::Test::Cluster;
 use PostgreSQL::Test::Utils;
 use Test::More;
 
@@ -15,27 +13,30 @@ program_help_ok('pg_subscriber');
 program_version_ok('pg_subscriber');
 program_options_handling_ok('pg_subscriber');
 
-my $tempdir = PostgreSQL::Test::Utils::tempdir;
+my $datadir = PostgreSQL::Test::Utils::tempdir;
 
-my $node = PostgreSQL::Test::Cluster->new('publisher');
-$node->init(allows_streaming => 'logical');
-$node->start;
-
-$node->command_fails_like(
-	['pg_subscriber'],
-	qr/no subscriber data directory specified/,
-	'target directory must be specified');
-$node->command_fails_like(
-	['pg_subscriber', '-D', $tempdir],
-	qr/no publisher connection string specified/,
-	'publisher connection string must be specified');
-$node->command_fails_like(
-	['pg_subscriber', '-D', $tempdir, '-P', 'dbname=postgres'],
-	qr/no subscriber connection string specified/,
-	'subscriber connection string must be specified');
-$node->command_fails_like(
-	['pg_subscriber', '-D', $tempdir, '-P', 'dbname=postgres', '-S', 'dbname=postgres'],
-	qr/is not a database cluster directory/,
-	'directory must be a real database cluster directory');
+command_fails(['pg_subscriber'],
+	'no subscriber data directory specified');
+command_fails(
+	[
+		'pg_subscriber',
+		'--pgdata', $datadir
+	],
+	'no publisher connection string specified');
+command_fails(
+	[
+		'pg_subscriber',
+		'--pgdata', $datadir,
+		'--publisher-conninfo', 'dbname=postgres'
+	],
+	'no subscriber connection string specified');
+command_fails(
+	[
+		'pg_subscriber',
+		'--pgdata', $datadir,
+		'--publisher-conninfo', 'dbname=postgres',
+		'--subscriber-conninfo', 'dbname=postgres'
+	],
+	'no database name specified');
 
 done_testing();
