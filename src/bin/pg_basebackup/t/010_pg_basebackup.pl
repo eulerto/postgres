3883dbd3bc6d2ae1e839623aaffc2f59a264a0c2
Euler Taveira <euler@eulerto.com>
2024-02-22 16:15:29 -0300

    set FATAL equals to all

--- src/bin/pg_basebackup/t/010_pg_basebackup.pl
+++ src/bin/pg_basebackup/t/010_pg_basebackup.pl
@@ -2,7 +2,7 @@
 # Copyright (c) 2021-2024, PostgreSQL Global Development Group
 
 use strict;
-use warnings;
+use warnings FATAL => 'all';
 use Config;
 use File::Basename qw(basename dirname);
 use File::Path     qw(rmtree);
